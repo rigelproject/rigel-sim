fd351655aeaaac7cb9a1cd152740c0165540e8f3
djohns53 <djohns53@illinois.edu>
2012-03-16 17:47:28 -0500

    attempt mshr minor cleanup

--- rigel-sim/include/mshr.h
+++ rigel-sim/include/mshr.h
@@ -67,9 +67,12 @@ class ReadyLinesEntry
     }
     //Match only on address
     bool operator==(const ReadyLinesEntry &other) const { return (addr == other.getAddr()); }
+
   private:
+
     //The address which will be ready
     uint32_t addr;
+
     //The absolute cycle number when it will be ready
     //Making it an absolute number instead of a "cycles from now" number avoids per-cycle decrements
     //And allows for easier debugging if something gets stuck.
@@ -77,16 +80,20 @@ class ReadyLinesEntry
 };
 
 template <int linesize>
-class MissHandlingEntry
-{  
+class MissHandlingEntry {  
+
   public: /* ACCESSORS */
+
     // Specify owner for cluster cache MSHRs.
     MissHandlingEntry(ClusterLegacy *owner = NULL);
+
     // Pass a pointer to the owning cache.
     // The MSHR will update cache-level valid bits and so forth when its methods are called.
     void init(int _id, DynamicBitset *db) { id = _id; validBits = db; }
+
     // Dump state for debugging purposes
     void dump() const;
+
     // IsRead is used to deterimine whether this is a read or a
     // write request.  All IC_MSG types are covered so that if something fishy
     // happens, we will find out about it sooner rather than just returning when
@@ -94,12 +101,16 @@ class MissHandlingEntry
     // Base choice on IC MSG since this is set for all valid entries.
     bool was_ccache_hwprefetch() const;
     bool was_gcache_hwprefetch() const;
+
     // Change the message type associated with the MSHR.
     void SetICMsgType(icmsg_type_t mt);
+
     // Return the message type associated with the MSHR.
     icmsg_type_t GetICMsgType() const { return _ic_msg_type; }
+
     // Track the first time this MSHR is valid.  Only used for stat collection.
     void set_first_attempt_cycle(uint64_t cycle) { first_attempt_cycle = cycle; }
+
     // Only keep the tag saved sets the members of a miss handling entry tracks
     // the address of request, number of cycles until ready, whether its a read
     // or write, and whether latency is variable
@@ -107,29 +118,38 @@ class MissHandlingEntry
       icmsg_type_t ic_type, int core_id, int pindex, int tid);
     void set(std::set<uint32_t> const & _addrs, int ready_cycles, bool vlatency,
              icmsg_type_t ic_type, int core_id, int pindex, int tid);
+
     // Check the tag to see if it is valid in this entry
     bool check_valid(uint32_t addr) const;
     bool match(uint32_t addr) const;
     bool all_lines_ready() const;
+
     // Fast check to see if entry is valid at all regardless of tag
     bool check_valid() const { return valid; }
+
     // Make the MSHR available, but do not reset it since the request only *might* have completed.
     // As a corollary, don't clear the pending_cores bit vector because we may do an unclear() later
     // and we don't want that information to be wiped out.
     void clear(bool mustBeDone = true);
+
     // Clear the value after checking the address.
     void clear(uint32_t addr) { if (check_valid(addr)) { clear(); } }
+
     // Note: When doing a writeback, we may want to invalidate the MSHR
     // temporarily so that we can reuse it.
     void unclear();
+
     // Note: When doing a writeback, we may want to invalidate the MSHR
     // temporarily so that we can reuse it.
     void unclear(uint32_t addr) { if (check_valid(addr)) { unclear(); } }
+
     // Next-level cache system has acknowledged this level cache's request
     void SetRequestAcked() { done_cache_ack = true; }
     bool IsRequestAcked() const { return done_cache_ack; }
+
     // Return true if the MSHR is going to read from memory (non exclusive of write).
     bool IsRead() const { return (ICMsg::check_is_read(GetICMsgType())); }
+
     // Return the address of the miss.
     //FIXME not sure if we want this interface to be here,
     //but there are a lot of places where the MSHR really should only have one address, and it doesn't
@@ -138,12 +158,15 @@ class MissHandlingEntry
     std::set<uint32_t>::const_iterator get_pending_addrs_begin() const { return addrs.begin(); }
     std::set<uint32_t>::const_iterator get_pending_addrs_end() const { return addrs.end(); }
     std::set<uint32_t> &get_addrs() { return addrs; }
+
     // Return the address to be filled.
     // On a BulkPF, this is the front of the priority queue of ready lines.
     // Otherwise, it is just normal get_addr()
     uint32_t get_fill_addr() const;
+
     // Are one or more lines waiting to be filled into the cache?
     bool has_ready_line() const;
+
     // Tell the MSHR that a fill has occurred for the specified address
     // TODO: We might not want to perform fills in FIFO order, to prevent HOLB in
     // the ready lines queue.  For instance, if two requests come in A, B, and
@@ -151,15 +174,19 @@ class MissHandlingEntry
     // want to fill B first.  For now, enforce FIFO order.
     void notify_fill(uint32_t addr);
     bool IsDone() const { return (addrs.empty() && ready_lines.empty()); }
+
     // Set the new address of the miss.
     // FIXME figure out a cleaner way for bulk prefetches to poke in here and
     // set some state in the MSHR
     // to tell whatever is looking at them that the reason they are "ready" now
     // is because of address X.
+
     // Return the requesting core number.
     int GetCoreID() const { return _core_id; }
+
     // Return the requesting thread ID
     int GetThreadID() const { return _tid; }
+
     // get the requesting L1DCache ID
     // this is the same as the core ID for one L1D per core
     // this is the same as the ThreadID for one L1D per thread
@@ -177,8 +204,10 @@ class MissHandlingEntry
     bool GetFillBlocked() const { return fill_blocked; }
     void SetFillBlocked() { fill_blocked = true; }
     void ClearFillBlocked() { fill_blocked = false; }
+
     // Allow operations to not cache at the global cache when bit is set
     bool GetAllowGlobalAlloc() const { return ICMsg::check_can_gcache_fill(GetICMsgType()); }
+
     // We must keep a bit around for outstanding requests that get probed while
     // they are pending at the Cluster Cache.  Right now this only occurs on
     // read->write upgrades.  We service the probe and taint the MSHR.  When the
@@ -191,6 +220,7 @@ class MissHandlingEntry
     bool get_probe_nak_bit() const { return probe_nak_bit; }
     void set_probe_nak_bit() { probe_nak_bit = true; }
     void clear_probe_nak_bit() { probe_nak_bit = false; }
+
     // Used for cluster cache MSHRs to indicate that another core (besides the one which
     // originally initiated the request) is waiting on the result.  This call will put the
     // core to sleep, and it will be woken up on a clear() operation.
@@ -203,113 +233,143 @@ class MissHandlingEntry
         pending_cores.set(i); owning_cluster->SleepCore(i);
       }
     }
+
     // Return/set the logical channel associated with the C$/G$ pair for this mshr
     void set_seq(int chan, seq_num_t seq) { seq_channel = chan; seq_num = seq; }
     int get_seq_channel() const { return seq_channel; }
     seq_num_t get_seq_number() const { return seq_num; }
     void wake_cores_all();
+
     // Check if the request generating this MSHR was a non blocking atomic,
     // i.e., it should not set the GlobalMemOp state.
     bool get_was_nonblocking_atomic() const { return was_nonblocking_atomic; }
     void set_nonblocking_atomic() { was_nonblocking_atomic = true; }
+
     // Notify the MSHR that the line at addr will be ready for a fill at cycle #ready_cycle
     void set_line_ready(uint32_t addr, uint64_t ready_cycle);
     void remove_addr(uint32_t addr) { addrs.erase(mask_addr(addr)); }
     static uint32_t mask_addr(uint32_t addr) { return addr & ~((uint32_t)linesize-1); }
+
     // Manipulate the incoherent bit.
     bool get_incoherent() const { return incoherent; }
     void set_incoherent() { incoherent = true; }
+
     // Interface for accessing the broadcast valid bit to signal the directory
     // that a broadcast occured to a valid line.
     bool get_bcast_resp_valid() const { return bcast_was_valid; }
     void set_bcast_resp_valid() { bcast_was_valid = true; }
     void clear_bcast_resp_valid() { bcast_was_valid = false; }
+
     // There is going to need to be a writeback associated with this MSHR.
     void set_bcast_write_shootdown(int o, uint32_t a);
     void clear_bcast_write_shootdown_required() { bcast_write_shootdown_required = false; }
     bool get_bcast_write_shootdown_required() const { return bcast_write_shootdown_required; }
     uint32_t get_bcast_write_shootdown_addr() const { return bcast_write_shootdown_addr; }
     int get_bcast_write_shootdown_owner() const { return bcast_write_shootdown_owner; }
+
     // flag for arb status used by mshr, similar to thread_arb_complete[] at cluster
     bool IsArbComplete() { return arb_complete; }
     void SetArbComplete() { arb_complete = true; }
+
     // helper function to sanity check mshr contents
     void sanity_check();
 
   public: /* DATA */
+
     // The message was taken by the next level of the cache.
     bool done_cache_ack;
+
     // The tag is the canonical comparison value.  The addr is the actual
     // address that generated the miss.  It is not used and should likely be
     // replaced by the bit vector representing words valid in the MSHR.
     uint32_t _tag;
+
     // How many cycles before line can be accessed.  Decremented in PerCycle()
     // for each level of the cache.
     // The type of request injected into the network
     icmsg_type_t _ic_msg_type;
+
     // The instruction which brought about the allocation of this MSHR.
     InstrLegacy *instr;
+
     // SIM_CYCLE where various actions occured for debugging.
     uint64_t first_attempt_cycle;
     uint64_t watchdog_last_set_time;
+
     // The core ID associated with the miss.  It may be invalid for non-core
     // requests, e.g., HW prefetches.
     int _core_id;
+
     // ditto, the ThreadID
     int _tid;
+
     // Set when the MSHR does not fill into the cluster cache on ACK
     int pending_index;
+
     // If the MSHR holds valid data, but it was blocked due to an eviction, it
     // will need to be tried again, but this gets it out of the network at
     // least.
     bool fill_blocked;
+
     // WDTs for scheduling and ack timeouts.
     int sched_WDT;
     int ack_WDT;
+
     // Set when a coherence probe comes in for the line.
     bool probe_poison_bit;
     bool probe_nak_bit;
+
     // Set on a write that finds the line in the cache, but read-only.  When the
     // read_release comes back, the controller will generate a write request.
     // Generally this should only be set in cache_model_write when a line is
     // found valid on a write attempt.
     bool read_to_write_upgrade;
+
     // Pointer to the cluster of which this MSHR is a part.
     // If the MSHR is not part of the cluster cache, this will be NULL.
     // This pointer is used to wake and sleep cores while they wait on a C$ miss.
     ClusterLegacy *owning_cluster;
+
     // Bitvector of all cores in the cluster waiting on this MSHR to complete.
     // This vector is only meaningful for cluster cache MSHRs.
     // It is used to wake up all pending cores when the MSHR is clear()ed
     std::bitset<rigel::CORES_PER_CLUSTER> pending_cores;
+
     // Addresses this MSHR is waiting on
     std::set<uint32_t> addrs;
     std::set<ReadyLinesEntry> ready_lines;
 
-
   private: /* Data */
+
     // Which MSHR am I?
     int id;
     DynamicBitset *validBits;
+
     // Set if there is a valid entry in this MHSR
     bool valid;
+
     // Sequence  numbers used for limited directories.  
     int seq_channel;
     seq_num_t seq_num;
+
     // Should the value that is outstanding for this request be kept incoherent
     // when it fills into (presumably) the cluster cache?
     bool incoherent;
     bool was_nonblocking_atomic;
+
     // CacheBase is our friend.  Everyone else needs to keep their mitts off
     // our private data.
     template <int ways, int sets, int cachelinesize, int MAX_OUTSTANDING_MISSES,
     int WB_POLICY, int CACHE_EVICTION_BUFFER_SIZE> friend class CacheBase;
+
     // Bit is set if the request should have the bcast_valid bit set in the ICMsg.
     bool bcast_was_valid;
+
     // Writeback required for the line.
     bool bcast_write_shootdown_required;
     uint32_t bcast_write_shootdown_addr;
     int bcast_write_shootdown_owner;
+
     // arbitration status flag for next level
     bool arb_complete;
     
@@ -356,8 +416,7 @@ MissHandlingEntry<linesize>::MissHandlingEntry(ClusterLegacy *owner) :
 ////////////////////////////////////////////////////////////////////////////////
 template <int linesize>
 inline void
-MissHandlingEntry<linesize>::dump() const
-{
+MissHandlingEntry<linesize>::dump() const {
   fprintf(stderr, "MSHR DUMP() ");
   fprintf(stderr, "valid: %1d ", valid);
   fprintf(stderr, "incoherent: %1d ", get_incoherent());
@@ -397,8 +456,7 @@ MissHandlingEntry<linesize>::dump() const
 ////////////////////////////////////////////////////////////////////////////////
 template <int linesize>
 inline bool
-MissHandlingEntry<linesize>::was_ccache_hwprefetch() const
-{
+MissHandlingEntry<linesize>::was_ccache_hwprefetch() const {
   return ((GetICMsgType() == IC_MSG_CCACHE_HWPREFETCH_REQ) ||
           (GetICMsgType() == IC_MSG_CCACHE_HWPREFETCH_REPLY));
 }
@@ -408,8 +466,7 @@ MissHandlingEntry<linesize>::was_ccache_hwprefetch() const
 ////////////////////////////////////////////////////////////////////////////////
 template <int linesize>
 inline bool
-MissHandlingEntry<linesize>::was_gcache_hwprefetch() const
-{
+MissHandlingEntry<linesize>::was_gcache_hwprefetch() const {
   return ((GetICMsgType() == IC_MSG_GCACHE_HWPREFETCH_REQ) ||
           (GetICMsgType() == IC_MSG_GCACHE_HWPREFETCH_REPLY));
 }
@@ -442,10 +499,9 @@ set(uint32_t addr, int ready_cycles, bool vlatency,
   // Sanity checks.
   assert(ic_type != IC_MSG_NULL);
   assert(ic_type != IC_INVALID_DO_NOT_USE);
-  if(owning_cluster != NULL)
-  {
+  if (owning_cluster != NULL) {
     if (!valid) { pending_cores.reset(); }
-    if(core_id >= 0 && core_id < (int)pending_cores.size() &&
+    if (core_id >= 0 && core_id < (int)pending_cores.size() &&
       ((ic_type == IC_MSG_READ_REQ) ||
       (ic_type == IC_MSG_WRITE_REQ) ||
       (ic_type == IC_MSG_GLOBAL_READ_REQ) ||
@@ -457,8 +513,7 @@ set(uint32_t addr, int ready_cycles, bool vlatency,
     }
   }
 
-  if(valid)
-  {
+  if (valid) {
     rigel::GLOBAL_debug_sim.dump_all(); rigel_dump_backtrace();
     dump();
     assert(0 && "Don't set() MSHRs that are already valid");
@@ -469,25 +524,21 @@ set(uint32_t addr, int ready_cycles, bool vlatency,
   DEBUG_HEADER();
   cerr << "\nMSHR Core " << core_id << " Thread " << tid << " Setting validBit ID " << id << std::endl;
 #endif
-  if(validBits != NULL)
+  if(validBits != NULL) {
     validBits->set(id);
-  
+  }
   addrs.clear();
   addrs.insert(mask_addr(addr));
 
-  if(!ready_lines.empty())
-  {
+  if(!ready_lines.empty()) {
     rigel::GLOBAL_debug_sim.dump_all(); rigel_dump_backtrace();
     dump();
     assert(0 && "ready_lines should be empty");
   }
 
-  if(vlatency) //not acked, the cache above must ack you
-  {
+  if(vlatency) { //not acked, the cache above must ack you 
     done_cache_ack = false;
-  }
-  else //already acknowledged and fills are pending
-  {
+  } else { //already acknowledged and fills are pending 
     done_cache_ack = true;
     set_line_ready(addr, rigel::CURR_CYCLE+ready_cycles);
   }
@@ -517,8 +568,7 @@ set(uint32_t addr, int ready_cycles, bool vlatency,
 
 template <int linesize>
 inline void
-MissHandlingEntry<linesize>::set_bcast_write_shootdown( int owner, uint32_t addr) 
-{
+MissHandlingEntry<linesize>::set_bcast_write_shootdown( int owner, uint32_t addr) {
   bcast_write_shootdown_owner = owner;
   bcast_write_shootdown_addr = addr;
   bcast_write_shootdown_required = true;
@@ -615,6 +665,7 @@ MissHandlingEntry<linesize>::clear(bool mustBeDone) //default value is true
   }
 }
 
+/// get_fill_addr
 template <int linesize>
 inline uint32_t 
 MissHandlingEntry<linesize>::get_fill_addr() const 
@@ -630,6 +681,7 @@ MissHandlingEntry<linesize>::get_fill_addr() const
   }
 }
 
+/// has_ready_line
 template <int linesize>
 inline bool 
 MissHandlingEntry<linesize>::has_ready_line() const 
@@ -638,6 +690,7 @@ MissHandlingEntry<linesize>::has_ready_line() const
   else { return (ready_lines.begin()->getReadyCycle() <= rigel::CURR_CYCLE); }
 }
 
+/// get_addr
 template <int linesize>
 inline uint32_t 
 MissHandlingEntry<linesize>::get_addr() const 
@@ -649,6 +702,7 @@ MissHandlingEntry<linesize>::get_addr() const
   return (*(addrs.begin()));
 }
 
+/// set_line_ready
 template <int linesize>
 inline void 
 MissHandlingEntry<linesize>::set_line_ready(uint32_t addr, uint64_t ready_cycle)
@@ -682,6 +736,7 @@ MissHandlingEntry<linesize>::set_line_ready(uint32_t addr, uint64_t ready_cycle)
   ready_lines.insert(rle);
 }
 
+/// notify fill
 template <int linesize>
 inline void 
 MissHandlingEntry<linesize>::notify_fill(uint32_t addr) 
